1d3c5e14103b8e7cb073a6a66799badef0941f91
basty <bastian.inostroza@gmail.com>
2019-05-21 02:41:17 -0400

    moved sequencenumber, and fixed troubles with base case

---
+++ objects/seqn.py
@@ -0,0 +1,36 @@
+from threading import Lock as Lock
+# Lock used based on https://stackoverflow.com/a/10525433
+
+
+class SequenceNumber:
+    def __init__(self, exp):  # Exponent it's for computing max sequence number
+        self.exp = exp
+        self.max = 10**exp - 1
+        self.number = 0
+        self.lock = Lock()
+
+    def next(self):
+        with self.lock:
+            print("Lock Acquired")
+            if self.number + 1 < self.max:
+                self.number += 1
+        return self.get()
+
+    def get(self):
+        with self.lock:
+            print("Lock Acquired by str")
+            # Returns number with exp - 1 characters
+            actual_exp = 0
+            num = self.number
+            while num > 0:
+                num /= 10
+                num = int(num)
+                actual_exp += 1
+            zeros_quantity = self.exp - actual_exp
+            string = ""
+            if self.number == 0:
+                zeros_quantity -= 1
+            while zeros_quantity > 0:
+                string += "0"
+                zeros_quantity -= 1
+            return string + str(self.number)
