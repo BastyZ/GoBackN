f2890b0ef92178cd89ccc5cb2a146d2ee76db324
mr <aege21@gmail.com>
2019-05-23 20:57:12 -0400

    renamed sequence_number to sequence_digits at server.py

--- server.py
+++ server.py
@@ -3,16 +3,16 @@ from threads.receiver import Receiver
 
 
 class Server:
-    def __init__(self, dest_ip, filename, window_size, package_size, sequence_number, receive_port, send_port):
+    def __init__(self, dest_ip, filename, window_size, package_size, sequence_digits, receive_port, send_port):
         self.dest_ip = dest_ip
         self.filename = filename
         self.window_size = window_size
         self.package_size = package_size
-        self.sequence_number = sequence_number
+        self.sequence_number = sequence_digits
         self.receive_port = receive_port
         self.send_port = send_port
 
-        self.sender = Sender(filename, dest_ip, send_port, window_size, package_size, sequence_number)
+        self.sender = Sender(filename, dest_ip, send_port, window_size, package_size, sequence_digits)
         self.receiver = Receiver(receive_port)
 
     def run(self):
